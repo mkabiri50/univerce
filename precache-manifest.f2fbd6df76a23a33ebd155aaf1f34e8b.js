b2eafee710774eb7c303380a24661d0f82ac00bf
unknown <mkabiri50@yahoo.com>
2019-11-24 08:05:54 -0800

    Updates

---
+++ precache-manifest.f2fbd6df76a23a33ebd155aaf1f34e8b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5f9cd60d663a18b68623edf114392b",
+    "url": "/univerce/index.html"
+  },
+  {
+    "revision": "eedcd4b6c2f602d39e05",
+    "url": "/univerce/static/css/2.3291aa16.chunk.css"
+  },
+  {
+    "revision": "bade9d96084e3358e05a",
+    "url": "/univerce/static/css/main.adc0866a.chunk.css"
+  },
+  {
+    "revision": "eedcd4b6c2f602d39e05",
+    "url": "/univerce/static/js/2.a2d80f55.chunk.js"
+  },
+  {
+    "revision": "bade9d96084e3358e05a",
+    "url": "/univerce/static/js/main.80f6e89f.chunk.js"
+  },
+  {
+    "revision": "dc90effd8789edc39fa4",
+    "url": "/univerce/static/js/runtime-main.f6c5ce4e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/univerce/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/univerce/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/univerce/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/univerce/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/univerce/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
